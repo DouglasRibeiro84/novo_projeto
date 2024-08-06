c7948194c483b050226330da43fb637972ef08b0
doouglas <douglasribeiro084@gmail.com>
2024-08-06 19:53:41 -0300

    aplicado as mascaras e validação

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    
-})
+
+    $('#telefone').mask('(00)00000-0000')
+    $(".money").mask("#.##0,00", { reverse: true });
+    $("#campo-c").mask('##0,0%', {
+        reverse: true,
+        translation: {
+        '#': { pattern: /[0-9]/, optional: true },
+        '0': { pattern: /[0-9]/ }
+        }
+    });
+    $('#campo-d').mask('00')
+
+    $('#form').validate({
+        rules: {
+            nome:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            nome: 'Este campo é obrigatório!',
+            email: 'Este campo é obrigatório!',
+        },
+    })
+});
