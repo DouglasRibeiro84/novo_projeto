b0cdedd9b07fc6e106024422036ca0005c8877f8
doouglas <douglasribeiro084@gmail.com>
2024-08-07 10:23:55 -0300

    finalizado a calculadora

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-
-    $('#telefone').mask('(00)00000-0000')
+    validacao()
+    calculadora()
+    
+    function validacao (){
+        $('#telefone').mask('(00)00000-0000')
     $(".money").mask("#.##0,00", { reverse: true });
     $("#campo-c").mask('##0,0%', {
         reverse: true,
@@ -10,7 +13,7 @@ $(document).ready(function(){
         }
     });
     $('#campo-d').mask('00')
-
+    
     $('#form').validate({
         rules: {
             nome:{
@@ -26,4 +29,39 @@ $(document).ready(function(){
             email: 'Este campo é obrigatório!',
         },
     })
+    }
+    function calculadora() {
+        $('#formulario').on('submit', function(e) {
+            e.preventDefault();
+    
+            function desformatarParaNumero(valor) {
+                return parseFloat(valor.replace(/\./g, '').replace(',', '.')) || 0;
+            }
+    
+            function desformatarTaxaJuros(valor) {
+                return parseFloat(valor.replace(',', '.').replace('%', '')) / 100 || 0;
+            }
+    
+            function formatarParaReais(valor) {
+                return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor);
+            }
+    
+            const valorInicial = desformatarParaNumero($('#campo-a').val());
+            const valorMensal = desformatarParaNumero($('#campo-b').val());
+            const taxaMensal = desformatarTaxaJuros($('#campo-c').val());
+            const meses = parseFloat($('#campo-d').val()) || 0;
+    
+            function calcularMontanteFinal(valorInicial, valorMensal, taxaMensal, meses) {
+                let montanteFinal = valorInicial;
+                for (let i = 0; i < meses; i++) {
+                    montanteFinal += valorMensal;
+                    montanteFinal *= (1 + taxaMensal);
+                }
+                return montanteFinal;
+            }
+            const montanteFinal = calcularMontanteFinal(valorInicial, valorMensal, taxaMensal, meses);
+            $('#valor-final').text(formatarParaReais(montanteFinal));
+            $('#reespota').show();
+        });
+    }
 });
